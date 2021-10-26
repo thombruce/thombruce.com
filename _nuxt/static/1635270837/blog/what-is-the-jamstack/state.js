4c35961dbabb153bf4a10cc9dcf7bfa08539f914
thombruce <thombruce@users.noreply.github.com>
2021-10-26 19:31:42 +0000

    deploy: 16da477fba0d0149f75fb484dee32120d9d0a807

--- _nuxt/static/1635270837/blog/what-is-the-jamstack/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635270837",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fwhat-is-the-jamstack",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b855e141"}}}}(null));
