cef64c0cbdbf5d1534113139ce1be42c73138b77
Thom Bruce <thom@thombruce.com>
2021-11-01 22:27:04 +0000

    Add code page with GitHub API content

---
+++ pages/code.vue
@@ -0,0 +1,116 @@
+<template lang='pug'>
+article
+  h2 Activity
+  .table-container
+    table
+      thead
+        tr
+          th Type
+          th Repo
+          th Author
+          th Time
+      tbody
+        tr(v-for='(event, i) in events')
+          td {{ event.type }}
+          td
+            a(:href="'https://github.com/' + event.repo.name") {{ event.repo.name }}
+          td
+            .avatar.mr-1
+              .mb-8.rounded-full.w-6.h-6
+                img.inline-block.align-middle(:src='event.actor.avatar_url')
+            span.inline-block.align-middle {{ event.actor.login }}
+          td {{ event.created_at }}
+  h2 Repositories
+  .table-container
+    table
+      thead
+        tr
+          th Name
+          th Description
+          th Language
+          th.text-center Git
+          th.text-center Issues
+      tbody
+        tr(v-for='(repo, i) in repos')
+          th
+            a(v-if='repo.homepage' :href='repo.homepage') {{ repo.name }}
+            span(v-else) {{ repo.name }}
+          td {{ repo.description }}
+          td {{ repo.language }}
+          td.text-center
+            a.text-lg(:href='repo.html_url')
+              fa(:icon='faCodeBranch')
+          td.text-center
+            a.text-lg(:href="repo.html_url + '/issues'")
+              .indicator
+                .indicator-item.badge.badge-error {{ repo.open_issues_count }}
+                fa(:icon='faBug')
+</template>
+
+<script>
+import { faCodeBranch, faBug } from '@fortawesome/free-solid-svg-icons'
+export default {
+  layout: 'modern',
+  async asyncData ({ $axios }) {
+    const events = await $axios.$get('https://api.github.com/users/thombruce/events?per_page=100')
+    const repos = await $axios.$get('https://api.github.com/users/thombruce/repos?per_page=100&type=public&sort=updated')
+    
+    return { events, repos }
+  },
+  async created () {
+    this.events = await this.$axios.$get('https://api.github.com/users/thombruce/events?per_page=100')
+    this.repos = await this.$axios.$get('https://api.github.com/users/thombruce/repos?per_page=100&type=public&sort=updated')
+  },
+  computed: {
+    faCodeBranch () {
+       return faCodeBranch
+    },
+    faBug () {
+      return faBug
+    }
+  },
+  head () {
+    return {
+      titleTemplate: null
+    }
+  }
+}
+</script>
+
+<style lang='postcss' scoped>
+.table-container {
+  @apply w-full h-full overflow-auto;
+  & table {
+    @apply table w-full h-full;
+    & th {
+      &:first-child {
+        @apply relative sm:sticky;
+      }
+    }
+    & thead {
+      & th {
+        @apply sticky;
+        top: 0;
+        z-index: 20;
+        &:first-child {
+          @apply sticky left-auto sm:left-0;
+          z-index: 30;
+        }
+      }
+    }
+    & thead, & tfoot {
+      & th, & td {
+        &:first-child {
+          @apply rounded-l-none;
+        }
+        &:last-child {
+          @apply rounded-r-none;
+        }
+      }
+    }
+    & td {
+      @apply whitespace-normal;
+    }
+  }
+}
+</style>
