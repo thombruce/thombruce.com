323b8b6cc6a7b10ad36c375dcf08eb7b35986e81
Thom Bruce <thom@thombruce.com>
2021-11-03 19:49:24 +0000

    Restyle hero

--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,14 @@
 <template lang='pug'>
 div
   .hero.min-h-screen.bg-base-200
-    .hero-content
-      div
-        h1 Hi there, I'm Thom!
-        p Welcome to thombruce.com.
+    .hero-content(class='flex-col lg:flex-row-reverse')
       figure
         NuxtImg(src='anywhere.png' width='400')
         figcaption.text-center.text-xs
           a(href="https://storyset.com/people" target='_blank') Illustration by Storyset
+      div
+        h1 Hi there, I'm Thom!
+        p Welcome to thombruce.com.
   article.max-w-prose.mx-auto
     TntBlogList(v-if='content' :articles='content')
 </template>
