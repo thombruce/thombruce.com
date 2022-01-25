4e6d7b1d16cca534da80fc74311cdbaccce29ed7
thombruce <thombruce@users.noreply.github.com>
2022-01-25 20:28:52 +0000

    deploy: cd3d30897291b96dffca9378461c8110e5583a59

--- _nuxt/static/1643122410/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643122410",layout:"default",error:a,serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c9120ac3"}},colorMode:{preference:d,value:d,unknown:b,forced:false}}}(null,true,"\u002F","system"));
