81617b25c4212f8075c87ab1f42fcd460ba39081
Thom Bruce <thom@thombruce.com>
2022-01-26 18:01:37 +0000

    Add screenplays

---
+++ content/blog/hello-screenplays.md
@@ -0,0 +1,11 @@
+---
+id: 3033DC04-52E5-4E63-B90F-FB68A3E37500
+title: Hello, Screenplays!
+date: 2022-01-26T17:59:37Z
+---
+
+I've just added a bunch of old [screenplays](/screenplays) I wrote in college to the site. Happy reading!
+
+<!--more-->
+
+[Read them here!](/screenplays)
---
+++ pages/screenplays/index.vue
@@ -0,0 +1,18 @@
+<template lang='pug'>
+article
+  RockBandBlogList(:articles='articles')
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content }) {
+    const articles = await $content('screenplays')
+      .where({ draft: { $ne: true } })
+      .sortBy('date', 'desc')
+      .fetch()
+      .catch(() => {})
+
+    return { articles }
+  }
+}
+</script>
