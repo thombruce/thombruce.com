7ce1516f9b247d55ee11c7084119176d3f7a4e6a
Thom Bruce <thom@thombruce.com>
2022-01-29 19:03:32 +0000

    Switch Crypto page to client mode

--- pages/crypto.vue
+++ pages/crypto.vue
@@ -2,18 +2,33 @@
 div.py-10
   p.text-center.text-4xl
     RockBandSimpleIcon(icon='ethereum')
-    | {{ ether }}
+    | {{ balance }}
 </template>
 
 <script>
 export default {
-  async asyncData ({ $Web3 }) {
-    const web3 = new $Web3('https://mainnet.infura.io/v3/a185cad5258f4c89a0107035403eaa4e')
-
-    const wei = await web3.eth.getBalance('0x98cCAac584a012D27A67ABD1586868a1bc3b33e3')
-    const ether = web3.utils.fromWei(wei)
-
-    return { ether }
+  data () {
+    return {
+      web3: null,
+      wei: null
+    }
+  },
+  computed: {
+    balance () {
+      if (this.wei) return this.web3.utils.fromWei(this.wei)
+    }
+  },
+  mounted () {
+    this.initWeb3()
+    this.fetchBalance()
+  },
+  methods: {
+    initWeb3 () {
+      this.web3 = new this.$Web3(this.$Web3.givenProvider || 'https://mainnet.infura.io/v3/a185cad5258f4c89a0107035403eaa4e')
+    },
+    async fetchBalance () {
+      this.wei = await this.web3.eth.getBalance('0x98cCAac584a012D27A67ABD1586868a1bc3b33e3')
+    }
   }
 }
 </script>
