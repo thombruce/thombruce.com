588bd01929f8c81c134c4e3f36652ff21bc3998e
Thom Bruce <thom@thombruce.com>
2022-01-29 00:45:14 +0000

    Integrate Web3 and display Ether balance

---
+++ pages/crypto.vue
@@ -0,0 +1,19 @@
+<template lang='pug'>
+div.py-10
+  p.text-center.text-4xl
+    RockBandSimpleIcon(icon='ethereum')
+    | {{ ether }}
+</template>
+
+<script>
+export default {
+  async asyncData ({ $Web3 }) {
+    const web3 = new $Web3('https://mainnet.infura.io/v3/a185cad5258f4c89a0107035403eaa4e')
+
+    const wei = await web3.eth.getBalance('0x98cCAac584a012D27A67ABD1586868a1bc3b33e3')
+    const ether = web3.utils.fromWei(wei)
+
+    return { ether }
+  }
+}
+</script>
---
+++ plugins/web3.js
@@ -0,0 +1,5 @@
+import Web3 from 'web3'
+
+export default ({}, inject) => {
+  inject('Web3', Web3)
+}
