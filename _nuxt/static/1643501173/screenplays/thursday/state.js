c127a6dd40684fd0a78a632bea549ee29ae1a340
thombruce <thombruce@users.noreply.github.com>
2022-01-30 00:06:23 +0000

    deploy: 49ba8b310c530359b5f6fcc56d525149b5708a98

---
+++ _nuxt/static/1643501173/screenplays/thursday/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643501173",layout:"default",error:a,serverRendered:b,routePath:"\u002Fscreenplays\u002Fthursday",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f15e4e82"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
