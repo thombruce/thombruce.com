a7f6cd958e3bf16f3a795a32b169904fbdee3b82
Thom Bruce <thom@thombruce.com>
2022-01-30 18:47:09 +0000

    Upgrade @candyfloss/nuxt

--- pages/crypto.vue
+++ pages/crypto.vue
@@ -4,11 +4,13 @@ div.py-10
     RockBandSimpleIcon(icon='ethereum')
     CandyFlossWalletBalance(
       address='0x98cCAac584a012D27A67ABD1586868a1bc3b33e3'
-      httpProvider='https://mainnet.infura.io/v3/a185cad5258f4c89a0107035403eaa4e'
     )
 </template>
 
 <script>
 export default {
+  created() {
+    this.$candyfloss.init('https://mainnet.infura.io/v3/a185cad5258f4c89a0107035403eaa4e')
+  }
 }
 </script>
