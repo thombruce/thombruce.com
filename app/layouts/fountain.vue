1e5cc7e9dc2f0c8e670370c6ae6039af79fbfb85
Thom Bruce <thom@thombruce.com>
2025-11-04 19:16:53 +0000

    Update @thombruce/fountainjs and remove margin patch

--- app/layouts/fountain.vue
+++ app/layouts/fountain.vue
@@ -3,9 +3,7 @@ const { page }: { page?: any } = useAttrs()
 </script>
 
 <template lang="pug">
-//- NOTE: screenplay margin should not be set by fountain.css
-//- We are overriding that default here.
-.screenplay(style="margin: 0 0;")
+.screenplay
   .title-page(v-html="page.body.html.title_page")
   .script(v-html="page.body.html.script")
 </template>
