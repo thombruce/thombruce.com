bfc073818a754f8b443872ec143e7b8116974182
Thom Bruce <thom@thombruce.com>
2025-11-14 03:49:45 +0000

    List blog posts in reverse order and as a list of title and paragraph blocks

--- app/layouts/list.vue
+++ app/layouts/list.vue
@@ -7,6 +7,7 @@ const { data: pages } = await useAsyncData(
     .where('path', 'LIKE', `${route.path}%`)
     .where('path', 'NOT LIKE', `${route.path}/%/%`)
     .where('path', 'NOT LIKE', `${route.path}`)
+    .order('date', 'DESC')
     .all()
 )
 </script>
@@ -14,9 +15,31 @@ const { data: pages } = await useAsyncData(
 <template lang="pug">
 article(class="prose dark:prose-invert max-w-none")
   slot/
-  ol(v-if="pages?.length")
-    li(v-for="page in pages")
-      NuxtLink(:to="page.path") {{ page.title }}
+  div(v-if="pages?.length" class="not-prose")
+    ol(class="space-y-4")
+      li(v-for="page in pages")
+        NuxtLink(:to="page.path" class="link-to-post")
+          h2(class="text-3xl") {{ page.title }}
+          p {{ page.description }}
   div(v-else)
     h1 No Posts Found
 </template>
+
+<style lang="postcss">
+/*
+   This first implementation looks correct
+   but isn't working. It is as if everything
+   up to 'dark' is being treated as the property,
+   instead of the apply rule just working.
+   TODO: Investigate
+ */
+a.link-to-post:hover h2 {
+  /* @apply text-zinc-800 dark:text-zinc-200; */
+  color: var(--color-zinc-900);
+}
+
+.dark a.link-to-post:hover h2 {
+  /* @apply text-zinc-800 dark:text-zinc-200; */
+  color: var(--color-zinc-100);
+}
+</style>
