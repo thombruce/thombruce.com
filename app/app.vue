391130d0d8e05e5573718f38918abe05ced93038
Thom Bruce <thom@thombruce.com>
2025-11-15 18:49:34 +0000

    Add CV link to footer with PDF icon

--- app/app.vue
+++ app/app.vue
@@ -35,6 +35,14 @@ div(class="w-[95vw] max-w-2xl mx-auto my-2")
             NuxtLink(to="https://linkedin.com/in/thombruce")
               span(class="icon-[simple-icons--linkedin] mr-2")/
               | LinkedIn
+          li
+            NuxtLink(to="https://github.com/thombruce/cv/releases/latest/download/Thom-Bruce_CV.pdf")
+              //- NOTE: Uses a little CSS magic to apply the appropriate icon
+              //- TODO: I don't necessarily love the proper use of custom attributes though;
+              //-       being "data-icon" instead of just "icon". Might revert to usage of a
+              //-       class. Must settle on this.
+              span(data-icon)/
+              | CV
     section(class="text-center")
       small Copyright Thom Bruce 1989 - 2025
 </template>
