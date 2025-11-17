8e7671fe71f03211164ec3a42a62f28eff11eaf7
Thom Bruce <thom@thombruce.com>
2025-11-17 10:58:29 +0000

    Add current weekday to homepage greeting

---
+++ app/components/global/Weekday.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+const today = ref(new Date())
+
+const weekday = ref(weekdays[today.value.getDay()])
+</script>
+
+<template lang="pug">
+time(:datetime="today.toISOString().substring(0, 10)") {{ weekday }}
+</template>
