7ada4826831e15b4fa28d507d71b105a8480e8c8
Thom Bruce <thom@thombruce.com>
2025-11-17 17:48:50 +0000

    Add link to main title

--- app/app.vue
+++ app/app.vue
@@ -3,7 +3,7 @@ div(class="w-[95vw] max-w-2xl mx-auto my-2")
   NuxtRouteAnnouncer/
 
   header(class="mb-4 space-y-2")
-    span(class="block text-4xl font-pixel") Thom Bruce
+    a(href="/" class="block text-4xl font-pixel") Thom Bruce
 
     nav(class="space-x-2")
       NuxtLink(to="/") Home
