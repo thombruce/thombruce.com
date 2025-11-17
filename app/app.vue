b29c28c22f2d79f8dc690ef451e4d2ce29087a84
Thom Bruce <thom@thombruce.com>
2025-11-17 17:10:40 +0000

    Add extra links and flex container to footer

--- app/app.vue
+++ app/app.vue
@@ -19,30 +19,43 @@ div(class="w-[95vw] max-w-2xl mx-auto my-2")
     NuxtPage/
 
   footer(class="mt-4")
-    section
-      h2(class="text-2xl font-pixel") Links
-      nav
-        ul
-          li
-            NuxtLink(to="https://github.com/thombruce")
-              span(class="icon-[simple-icons--github] mr-2")/
-              | GitHub
-          li
-            NuxtLink(to="https://bsky.app/profile/thombruce.com")
-              span(class="icon-[simple-icons--bluesky] mr-2")/
-              | Bluesky
-          li
-            NuxtLink(to="https://linkedin.com/in/thombruce")
-              span(class="icon-[simple-icons--linkedin] mr-2")/
-              | LinkedIn
-          li
-            NuxtLink(to="https://github.com/thombruce/cv/releases/latest/download/Thom-Bruce_CV.pdf")
-              //- NOTE: Uses a little CSS magic to apply the appropriate icon
-              //- TODO: I don't necessarily love the proper use of custom attributes though;
-              //-       being "data-icon" instead of just "icon". Might revert to usage of a
-              //-       class. Must settle on this.
-              span(data-icon)/
-              | CV
+    div(class="flex flex-wrap space-y-4")
+      section(class="flex-1 min-w-2xs")
+        h2(class="text-2xl font-pixel") Extra
+        nav
+          ul
+            li
+              NuxtLink(to="/screenplays")
+                span(class="icon-[fa7-solid--masks-theater] mr-2")/
+                | Screenplays
+            li
+              NuxtLink(to="https://github.com/thombruce/.dotfiles")
+                span(class="icon-[fa7-solid--screwdriver-wrench] mr-2")/
+                | .dotfiles
+            li
+              NuxtLink(to="https://github.com/thombruce/cv/releases/latest/download/Thom-Bruce_CV.pdf")
+                //- NOTE: Uses a little CSS magic to apply the appropriate icon
+                //- TODO: I don't necessarily love the proper use of custom attributes though;
+                //-       being "data-icon" instead of just "icon". Might revert to usage of a
+                //-       class. Must settle on this.
+                span(data-icon)/
+                | CV
+      section(class="flex-1 min-w-2xs")
+        h2(class="text-2xl font-pixel") Socials
+        nav
+          ul
+            li
+              NuxtLink(to="https://github.com/thombruce")
+                span(class="icon-[simple-icons--github] mr-2")/
+                | GitHub
+            li
+              NuxtLink(to="https://bsky.app/profile/thombruce.com")
+                span(class="icon-[simple-icons--bluesky] mr-2")/
+                | Bluesky
+            li
+              NuxtLink(to="https://linkedin.com/in/thombruce")
+                span(class="icon-[simple-icons--linkedin] mr-2")/
+                | LinkedIn
     section(class="text-center")
       small Copyright Thom Bruce 1989 - 2025
 </template>
