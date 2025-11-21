0294699bef911f9db3effa8e454ed69de0fc633d
Thom Bruce <thom@thombruce.com>
2025-11-21 17:06:03 +0000

    Add links to search results and filter out duplicates (where title matches header)

--- app/components/ModalView.vue
+++ app/components/ModalView.vue
@@ -13,7 +13,7 @@ div
   div(
     popover
     :id="`modal-${uid}`"
-    class="m-auto px-4 py-2 bg-light dark:bg-dark text-dark dark:text-light border"
+    class="max-w-full md:max-w-1/2 lg:max-w-1/3 xl:max-w-1/4 m-auto px-4 py-2 bg-light dark:bg-dark text-dark dark:text-light border"
   )
     slot/
 </template>
