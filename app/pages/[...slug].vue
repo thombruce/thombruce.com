775227d267a1382d858d0751a9a200671e3ab0d4
Thom Bruce <thom@thombruce.com>
2025-11-28 18:55:48 +0000

    Add vertical space between tags that rollover to new lines

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -31,17 +31,17 @@ NuxtLayout(:name="layout" :page="page" :list="page.meta?.list")
 
     div(v-if="page?.tags?.length" class="not-prose")
       h2 Tags
-      ul(class="space-x-2")
+      ul(class="space-x-2 space-y-2")
         li(v-for="tag in page.tags" class="inline-block bg-teal-400 dark:bg-teal-600 rounded px-2 font-bold before:content-['#']") {{ tag }}
 
     div(v-if="page?.contexts?.length" class="not-prose")
       h2 Mentions
-      ul(class="space-x-2")
+      ul(class="space-x-2 space-y-2")
         li(v-for="context in page.contexts" class="inline-block bg-rose-400 dark:bg-rose-600 rounded px-2 font-bold before:content-['@']") {{ context }}
 
     div(v-if="page?.projects?.length" class="not-prose")
-      h2 Projects
-      ul(class="space-x-2")
+      h2 Tasks
+      ul(class="space-x-2 space-y-2")
         li(v-for="project in page.projects" class="inline-block bg-amber-400 dark:bg-amber-600 rounded px-2 font-bold before:content-['+']") {{ project }}
 
   ContentRenderer(v-if="page" :value="page" class="content-renderer")/
