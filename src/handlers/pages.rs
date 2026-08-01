5049383880638d3559baa736fef46444803dc65b
Thom Bruce <thom@thombruce.com>
2026-08-01 09:56:59 +0100

    feat: serve /about route with basic info

---
+++ src/handlers/pages.rs
@@ -0,0 +1,18 @@
+pub async fn home() -> &'static str {
+r#"Thom Bruce
+Web and Software Developer
+Ruby / Rust / TypeScript
+
+Site Under Construction"#
+}
+
+
+pub async fn about() -> &'static str {
+r#"About
+i. Rubyist
+ii. Rustacean
+iii. TypeScripter
+
+Writing code for nearly 20 years."#
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 use axum::{
-    routing::get,
     Router,
+    routing::get,
 };
+
+mod handlers;
+use handlers::pages;
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async {
-        r#"Thom Bruce
-Web and Software Developer
-Ruby / Rust / TypeScript
-
-Site Under Construction"#
-    }));
+    let app = Router::new()
+        .route("/", get(pages::home))
+        .route("/about", get(pages::about));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
